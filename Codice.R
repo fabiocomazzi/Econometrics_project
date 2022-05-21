8afa44f9a18a76f91a0b775d9fb7aa67e401ecb2
LucaZ98 <lucazerman98@gmail.com>
2022-05-21 16:39:47 +0200

    Update Codice.R

--- Codice.R
+++ Codice.R
@@ -1,1 +1,11 @@
 # Qui scriviamo il nostro codice raga
+
+setwd("~/GitHub/Econometrics_project")
+rm(list = ls())
+
+library("readxl")
+library("lubridate")
+
+df = read_excel("dataset.xlsx", header = T)
+
+
